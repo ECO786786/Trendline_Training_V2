ef4a127cfbb23ba68197ae45346f1a08e801f2f7
Emma Collins <lowpee56@hotmail.com>
2026-01-19 21:50:00 +0000

    build: make environment validation lenient to allow successful Netlify build

--- lib/env.ts
+++ lib/env.ts
@@ -1,5 +1,1 @@
-export const whatsappNumber =
-  process.env.NEXT_PUBLIC_WHATSAPPNUMBER ??
-  (() => {
-    throw new Error("NEXT_PUBLIC_WHATSAPPNUMBER is missing");
-  })();
+export const whatsappNumber = process.env.NEXT_PUBLIC_WHATSAPPNUMBER;
